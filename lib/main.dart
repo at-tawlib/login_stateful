1237ed7c5d586ea7bac49773672eff0f69d01905
Abdul-Fatahu Hardi <a.fatahu95@gmail.com>
2023-04-11 08:21:35 +0000

    Initial Commit

---
+++ lib/main.dart
@@ -0,0 +1,12 @@
+/// 11-04-2023 Forms and Validation
+/// Stateful Management in flutter without using BLOC Pattern
+/// The disadvantage of this method is that, it does not scale to large
+/// However, it is easier to understand
+/// StatefulWidget, Column, Form, EdgeInsets, Container
+/// 
+import 'package:flutter/material.dart';
+import 'src/app.dart';
+
+void main() {
+  runApp(App());
+}
---
+++ lib/src/screens/login_screen.dart
@@ -0,0 +1,93 @@
+// Stateful, Column, EdgeInsets, Form, GlobalKey, Validator
+// ElevatedButton,
+import 'package:flutter/material.dart';
+
+class LoginScreen extends StatefulWidget {
+  @override
+  createState() {
+    return LoginScreenState();
+  }
+}
+
+class LoginScreenState extends State<LoginScreen> {
+  final formKey = GlobalKey<FormState>();
+
+  String email = '';
+  String password = '';
+
+  @override
+  Widget build(context) {
+    return Container(
+      margin: const EdgeInsets.all(20.0),
+      child: Form(
+        key: formKey,
+        child: Column(
+          children: [
+            Container(margin: const EdgeInsets.only(top: 25.0)),
+            emailField(),
+            passwordField(),
+            // use container to add space between widgets instead of adding the margin to the widget itself
+            Container(margin: const EdgeInsets.only(top: 25.0)),
+            submitButton(),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget emailField() {
+    return TextFormField(
+      keyboardType: TextInputType.emailAddress,
+      decoration: const InputDecoration(
+        labelText: 'Email Address',
+        hintText: 'you@example.com',
+      ),
+      validator: (value) {
+        if (value == null || value.isEmpty) {
+          return 'Please enter some text';
+        }
+        if (!value.contains('@')) {
+          //  return null if void otherwise (string with the error.message) if invalid
+          return 'Please enter a valid email';
+        }
+      },
+      onSaved: (value) {
+        email = value!;
+      },
+    );
+  }
+
+  Widget passwordField() {
+    return TextFormField(
+      obscureText: true,
+      decoration: const InputDecoration(
+        labelText: 'Enter Password',
+        hintText: 'Password',
+      ),
+      validator: (value) {
+        if (value == null) {
+          return "Please enter a password";
+        }
+        if (value.length < 4) {
+          return 'Password must be at least 4 characters';
+        }
+      },
+      onSaved: (value) {
+        password = value!;
+      },
+    );
+  }
+
+  Widget submitButton() {
+    return ElevatedButton(
+      child: const Text('Submit!'),
+      onPressed: () {
+        // formKey.currentState?.reset();
+        if (formKey.currentState!.validate()) {
+          formKey.currentState!.save();
+          print("Time to post $email and $password to my API");
+        }
+      },
+    );
+  }
+}
